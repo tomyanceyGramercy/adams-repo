44cc5f2ec59834abc802232d851c24affb332138
tomyanceyGramercy <65623232+tomyanceyGramercy@users.noreply.github.com>
2024-10-28 18:13:30 -0700

    added confetti

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import confetti from 'canvas-confetti';
+confetti();
 </script>
 
 <template>
-    <div>
-        <h1>Welcome to adams-repo</h1>
-        <HelloWorld msg="You did it!" />
-    </div>
+  <div>
+    <h1>Welcome to adams-repo</h1>
+    <HelloWorld msg="You did it!" />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
